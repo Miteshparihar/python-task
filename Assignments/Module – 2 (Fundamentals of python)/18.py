242970c0561e3f2b61b70021dca11014eb7087e7
Miteshparihar <pariharmitesh2@gmail.com>
2023-07-19 21:40:32 +0530

    merge

--- Assignments/Module – 2 (Fundamentals of python)/18.py
+++ Assignments/Module – 2 (Fundamentals of python)/18.py
@@ -2,5 +2,13 @@
 should be at least 3). If the given string already ends with 'ing' then add
 'ly' instead if the string length of the given string is less than 3, leave it
 unchanged. '''
-
+str1=str(input("enter a string:-"))
+length=len(str1)
+if length>2:
+    if str1[-3:]=='ing':
+        str1 +="ly"
+    else:
+        str1 +="ing"
+print(str1)
+        
 
