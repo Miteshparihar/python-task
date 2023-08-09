61c31f7fa24dc000bcb1e92bcfc32528b66fbe39
Miteshparihar <pariharmitesh2@gmail.com>
2023-08-09 14:58:24 +0530

    merge other

--- Daily_task/basic of py task/pattern task.py
+++ Daily_task/basic of py task/pattern task.py
@@ -12,6 +12,16 @@ print("-----------------------------------")
                            1 0 1
                            1 0 1 0
                            1 0 1 0 1   '''
+n =6  
+for i in range(1, n):
+    for j in range(1, i + 1):
+        if j%2==1:
+            print("1", end=" ")
+        else:
+            print("0",end=" ")
+    print()
+
+print("-----------------------------------")
 
 
 '''
@@ -20,7 +30,17 @@ print("-----------------------------------")
                            4 5 6
                            7 8 9 10 
 '''
+current_num = 1
+stop = 2
+rows = 4
 
+for i in range(rows):
+    for column in range(1, stop):
+        print(current_num, end=' ')
+        current_num += 1
+    print("")
+    stop += 1
+print("-----------------------------------")
 '''
   print                   A
                             B C
@@ -28,7 +48,16 @@ print("-----------------------------------")
                             G H I J
                             K L M N O
 '''
+ascii_number = 65
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        character = chr(ascii_number)
+        print(character, end=' ')
+        ascii_number += 1
+    print(" ")
 
+    
 print("--------------------------------------------------------------")
 
 
@@ -42,6 +71,13 @@ for i in range(1,11):
 
 ''' task:-2
             take user input as number and print tables upto the number'''
+user=int(input("Enter number and upto the table:-"))
+for i in range(1,user):
+    for j in range(1,11):
+        print(i,"x",j,"=",i*j)
+        
+        
+    
 
         
 
--- Daily_task/basic of py task/string task.py
+++ Daily_task/basic of py task/string task.py
@@ -2,10 +2,29 @@
 #task-1 (find length)
 st="Mitesh     mali"
 print(len(st))
+
+
+
 #task - 2(find character)
 hel="fjhdgiu efeufeufgueuiehy"
 print(hel.count("u"))
+
+
+
 #task-3 (count each character)
+string = "Hello good morning"
+all_char ={ }
+print(type(all_char))
+for i in string:
+    if i in all_char:
+        all_char[i] += 1
+    else:
+        all_char[i] = 1
+
+print("Count of all characters in Hello good morning is :",all_char)
+
+
+
 
 '''task : 4
 
@@ -15,16 +34,30 @@ Total Voewls :
 Total Consonants : 
 Total Digits : 
 Total White Spaces : '''
-count=0
-len= "mikfhuiwefgfhij"
-i=0
-if i<=len:
-    if:
-        str[i]='a','e','i','o','u'
-        count+=1
+#vowels or consonent
+user=input("Please enter a string as you wish: ");
+vowels=0
+consonants=0
+for i in user:
+    if(i == 'a'or i == 'e'or i == 'i'or i == 'o'or i == 'u' or
+       i == 'A'or i == 'E'or i == 'I'or i == 'O'or i == 'U' ):
+             vowels=vowels+1;
     else:
-        i+=1
-else:
-    count=0
-    break
+            consonants=consonants+1;
+print("The number of vowels:",vowels);
+print("The number of consonant:",consonants);
+#white space
+string= input("Check white space:-")
+count=0
+for i in range(0,len(string)):
+    if string[i]==" ":
+        count+=1
+print(count)
+
+#digit count
+
+
+
+
+
 
